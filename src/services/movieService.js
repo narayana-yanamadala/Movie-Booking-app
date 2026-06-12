ae6f2d88d5df5936d39d24b864ce2110e4b6df6f
Y Narayana <142072590+123Nara@users.noreply.github.com>
2026-06-12 18:24:58 +0530

    Connect frontend to Render backend

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://127.0.0.1:8000/api/movies/";
+const API_URL = "https://cinerush-backend.onrender.com/api/movies/";
 
 export const getMovies = async () => {
   try {
